b2c0923be4ab1817d5bc69f16501f73123dcab4a
Genna <wingertbe@gmail.com>
2017-08-17 04:58:35 +0200

    Remove generated code

--- MemoryClient.Web/extracted/Components/Veil.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-class Veil extends React.Component {
-    render() {
-        return (<div id="veil" style={this.divStyle}></div>);
-    }
-}
-export default Veil;
-//# sourceMappingURL=Veil.jsx.map
--- MemoryClient.Web/extracted/Styles.js
+++
@@ -1,7 +0,0 @@
-export default {
-    base: {},
-    bgPrimary: {
-        background: "#fdcc52"
-    }
-};
-//# sourceMappingURL=Styles.js.map
