546722121d0ef364766583c850b84f0aa365508b
Genna <wingertbe@gmail.com>
2017-08-17 04:55:38 +0200

    Remove generated code

--- MemoryClient.Web/dist/Components/Button.js
+++
@@ -1,3 +0,0 @@
-class Button {
-}
-//# sourceMappingURL=Button.js.map
--- MemoryClient.Web/dist/Styles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    base: {},
-    bgPrimary: {
-        background: "#fdcc52"
-    }
-};
-//# sourceMappingURL=Styles.js.map
