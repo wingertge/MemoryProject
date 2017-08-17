b30ac41dd8cbddbd8c7586d691b67b32b1272c70
Genna <wingertbe@gmail.com>
2017-08-17 05:44:23 +0200

    a

--- build.master.sh
+++ build.master.sh
@@ -2,7 +2,8 @@
 cd sln
 set -e
 dotnet restore MemoryProject.sln
-#rm -rf $(pwd)/publish/api
-#rm -rf $(pwd)/publish/web
+rm -rf $(pwd)/publish/api
+rm -rf $(pwd)/publish/web
+echo "test"
 dotnet publish MemoryServer/MemoryServer.csproj -c release -o $(pwd)/publish/api
 dotnet publish MemoryClient.Web/MemoryClient.Web.csproj -c release -o $(pwd)/publish/web
