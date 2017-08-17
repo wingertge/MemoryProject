982ba8c56fd11291c240d64efa3640c72355e3e2
Genna <wingertbe@gmail.com>
2017-08-17 05:18:01 +0200

    asd

--- build.master.sh
+++ build.master.sh
@@ -3,6 +3,6 @@ set -e
 dotnet restore MemoryProject.sln
 rm -rf $(pwd)/publish/api
 rm -rf $(pwd)/publish/web
-ls
+ls --color=never
 dotnet publish $(pwd)/MemoryServer/MemoryServer.csproj -c release -o $(pwd)/publish/api
 dotnet publish $(pwd)/MemoryClient.Web/MemoryClient.Web.csproj -c release -o $(pwd)/publish/web
