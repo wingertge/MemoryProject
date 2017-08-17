113ee6330eb98fa8742e7604500d913e763b82c5
Genna <wingertbe@gmail.com>
2017-08-17 05:22:00 +0200

    asd

--- build.master.sh
+++ build.master.sh
@@ -3,6 +3,7 @@ set -e
 dotnet restore MemoryProject.sln
 rm -rf $(pwd)/publish/api
 rm -rf $(pwd)/publish/web
-ls --color=never
+echo "Sleeping now..."
+sleep 60000
 dotnet publish $(pwd)/MemoryServer/MemoryServer.csproj -c release -o $(pwd)/publish/api
 dotnet publish $(pwd)/MemoryClient.Web/MemoryClient.Web.csproj -c release -o $(pwd)/publish/web
