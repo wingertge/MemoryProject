af1c487063bdc570813a13fc0cccc711e8c3256a
Genna <wingertbe@gmail.com>
2017-08-17 05:25:56 +0200

    a

--- build.master.sh
+++ build.master.sh
@@ -1,9 +1,10 @@
 #!bin/bash
 set -e
+ls
+echo "Sleeping now..."
+sleep 60000
 dotnet restore MemoryProject.sln
 rm -rf $(pwd)/publish/api
 rm -rf $(pwd)/publish/web
-echo "Sleeping now..."
-sleep 60000
 dotnet publish $(pwd)/MemoryServer/MemoryServer.csproj -c release -o $(pwd)/publish/api
 dotnet publish $(pwd)/MemoryClient.Web/MemoryClient.Web.csproj -c release -o $(pwd)/publish/web
