82b37eed059320c48f432d037b1d672d4d0270d1
Genna <wingertbe@gmail.com>
2017-08-18 10:58:15 +0200

    a

---
+++ MemoryServer/TempFix/GoogleCloudStorageCacheOptions.cs
@@ -0,0 +1,13 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+
+namespace CloudStorage.Wrapper
+{
+    public class GoogleCloudStorageCacheOptions
+    {
+        public GoogleCredential Credentials { get; set; }
+        public string BucketName { get; set; } = "dotnet-bucket";
+        public EncryptionKey EncryptionKey { get; set; } = null;
+        public string ProjectName { get; set; }
+    }
+}
