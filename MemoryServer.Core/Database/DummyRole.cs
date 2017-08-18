5e4c2e4659e9abcb2933beb33250e89b340ec666
Genna <wingertbe@gmail.com>
2017-08-18 08:36:23 +0200

    a

---
+++ MemoryServer.Core/Database/DummyRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace MemoryServer.Core.Database
+{
+    /** Fix your damn shit microsoft **/
+    public class DummyRole : IdentityRole<Guid>
+    {
+        
+    }
+}
---
+++ MemoryServer.Tests/AssignmentTests.cs
@@ -0,0 +1,31 @@
+﻿using MemoryServer.Core.Business;
+using MemoryServer.Core.Business.Impl;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching.Redis;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace MemoryServer.Tests
+{
+    public class AssignmentTests
+    {
+        [TestClass]
+        public class ReviewStoreTests
+        {
+            private readonly IReviewStore _reviewStore;
+            public ReviewStoreTests()
+            {
+                _reviewStore = new ReviewStore(new MemoryDistributedCache(new OptionsWrapper<MemoryDistributedCacheOptions>(new MemoryDistributedCacheOptions())));
+            }
+
+            [TestMethod]
+            public void EntityValid()
+            {
+                var result = JsonConvert.SerializeObject(new ReviewCardContext());
+                Assert.AreEqual(result, @"{""Hand"":[],""DiscardPile"":[],""UpcomingQueue"":[]}");
+            }
+        }
+    }
+}
